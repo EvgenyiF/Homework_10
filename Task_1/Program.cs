7ffa0eca9ee813a25b5d5dc731742bcd4745ef71
Evgeniy <jon83@inbox.ru>
2022-04-13 22:29:50 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные 
+//числа в промежутке от M до N в обратном порядке. M = 1; N = 5. -> «5, 4, 3, 2, 1»
+
+Console.Write("Задайте число M: ");
+int m = Convert.ToInt16(Console.ReadLine());
+Console.Write("Задайте число N: ");
+int n = Convert.ToInt16(Console.ReadLine());
+
+void NaturalNumber(int m, int n)
+{
+    if (m < n)
+    {
+        Console.Write($"{n} ");
+        NaturalNumber(m, n-1);
+    }
+    if (m > n)
+    {
+        Console.Write($"{m} ");
+        NaturalNumber(m-1, n);
+    }
+    if (m == n) Console.Write($"{n} ");
+}
+NaturalNumber(m, n);
